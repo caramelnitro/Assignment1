92ac159e36e6cb39cb17c05ea9219f958746eec6
Caleb <jmangrum5@my.apsu.edu>
2019-02-11 14:10:33 -0600

    Added Game activity and button functionality

---
+++ app/src/main/java/apsu/edu/assignment1/GameActivity.java
@@ -0,0 +1,13 @@
+package apsu.edu.assignment1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/apsu/edu/assignment1/MainActivity.java
+++ app/src/main/java/apsu/edu/assignment1/MainActivity.java
@@ -1,8 +1,8 @@
 package apsu.edu.assignment1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,10 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = findViewById(R.id.backButton);
+        Button b = findViewById(R.id.rules_button);
         b.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 Intent intent = new Intent(getApplicationContext(), RulesActivity.class);
+                startActivity(intent);
+            }
+        });
+        b = findViewById(R.id.play_button);
+        b.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
                 startActivity(intent);
             }
         });

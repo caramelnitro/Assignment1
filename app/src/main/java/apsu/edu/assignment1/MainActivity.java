aa796abc337fc4fb15111926c5a70dcab31a20dd
caramelnitro <caramelnitro@yahoo.com>
2019-02-11 13:29:06 -0600

    Added buttons

--- app/src/main/java/apsu/edu/assignment1/MainActivity.java
+++ app/src/main/java/apsu/edu/assignment1/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button b = findViewById(R.id.backButton);
         b.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){

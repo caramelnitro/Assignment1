da712420483173bcf13c51f39a60b7e748600f84
Caleb <jmangrum5@my.apsu.edu>
2019-02-12 13:44:46 -0600

    Fixed stuff

--- app/src/main/java/apsu/edu/assignment1/GameActivity.java
+++ app/src/main/java/apsu/edu/assignment1/GameActivity.java
@@ -3,7 +3,6 @@ package apsu.edu.assignment1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import java.util.Random;
@@ -11,14 +10,13 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     private boolean random = true;
     private boolean x = true;
+    private Random r = new Random();
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_game);
-        Random r = new Random();
         int rand = r.nextInt(10) + 1;
-
-        if(rand%2 == 0) {
+        if(rand % 2 == 0) {
             x = true;
         }
         else {
@@ -29,37 +27,85 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         final int buttonId = v.getId();
+
         if(random && findViewById(R.id.imageButton).getId() == buttonId){
-            if(x == true)
+            if(x) {
                 ((ImageButton) findViewById(R.id.imageButton)).setImageResource(R.drawable.letter_x);
-            else
+            }
+            else {
                 ((ImageButton) findViewById(R.id.imageButton)).setImageResource(R.drawable.letter_o);
-
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton2).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton2)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton2)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton3).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton3)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton3)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton4).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton4)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton4)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton5).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton5)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton5)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        if(random && findViewById(R.id.imageButton6).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton6)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton6)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton7).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton7)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton7)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton8).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton8)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton8)).setImageResource(R.drawable.letter_o);
+            }
         }
-        else if(random){
-
+        else if(random && findViewById(R.id.imageButton9).getId() == buttonId){
+            if(x) {
+                ((ImageButton) findViewById(R.id.imageButton9)).setImageResource(R.drawable.letter_x);
+            }
+            else {
+                ((ImageButton) findViewById(R.id.imageButton9)).setImageResource(R.drawable.letter_o);
+            }
+        }
+        int rand = r.nextInt(10) + 1;
+        if(rand % 2 == 0) {
+            x = true;
+        }
+        else {
+            x = false;
         }
     }
-
 }
